7e3bf64fb3a6c265007b7682c35acb312014354c
myrayzsiza <rayzanitah2@gmail.com>
2025-10-01 12:49:11 +0300

    review

---
+++ w05-product-review/js/main.js
@@ -0,0 +1,16 @@
+// Product Array
+const products = [
+    { id: 1, name: "Product A" },
+    { id: 2, name: "Product B" },
+    { id: 3, name: "Product C" },
+    { id: 4, name: "Product D" }
+];
+
+// Populate Product Select Options
+const productSelect = document.getElementById("product");
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.name; // value = product name
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+});
